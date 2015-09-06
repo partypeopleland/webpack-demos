a45609c709e38b420df37c5b8dca38ebc52d8c6a
art <partypeopleland@gmail.com>
2015-09-06 22:33:47 +0800

    test4

--- demo02/webpack.config.js
+++ demo02/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     loaders: [{ 
         test: /\.css$/, loader: "style!css!autoprefixer"
       },{
-       test: /\.jpg$/, loader: "url?limit=7000"
+       test: /\.jpg$/, loader: "url"
       }]
   }
 };
